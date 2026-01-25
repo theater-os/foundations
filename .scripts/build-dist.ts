8913a6878630a86bd375d052576fde6e9a553ce0
David Jegat <david.jegat@gmail.com>
2026-01-25 16:44:30 +0100

    chore: try to fix testing the return

--- .scripts/build-dist.ts
+++ .scripts/build-dist.ts
@@ -5,6 +5,7 @@ import packages from '../package.json'
 console.log(chalk.grey.bold('⚡️ Building dist...'))
 
 for await (const pkg of packages.workspaces.packages) {
+  console.log(chalk.grey.bold(`🔍 Building ${pkg}...`))
   await $`cd ${pkg} && bun i && bun build:dist`.catch(() => {
     console.log(chalk.yellow.bold(`🤔 ${pkg} dist build failed, skipping...`))
   })
