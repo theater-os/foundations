7c02b8a5ffbf7eba71bf3f96a0ba68abb8362af6
David Jegat <david.jegat@gmail.com>
2026-02-23 22:22:29 +0100

    feat: add supports for more than 10 matchers but not with a great inference

--- packages/match/src/match.ts
+++ packages/match/src/match.ts
@@ -152,6 +152,10 @@ export namespace Match {
     defaultMatcher: DefaultMatcher<R10>,
   ): (value: unknown) => R1 | R2 | R3 | R4 | R5 | R6 | R7 | R8 | R9 | R10
   // biome-ignore lint/suspicious/noExplicitAny: type inference is done above
+  export function of<const MS extends MatcherList<any, any>>(
+    ...matchers: MS
+  ): (value: unknown) => ReturnType<MS[number][1]>
+  // biome-ignore lint/suspicious/noExplicitAny: type inference is done above
   export function of<R>(...matchers: MatcherList<any, any>) {
     return (value: unknown): R => {
       for (const [predicate, result] of matchers) {
