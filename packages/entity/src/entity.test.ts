f96d3b0886cdf17b6d0b893529ca1388dba07a24
David Jegat <david.jegat@gmail.com>
2026-02-24 17:23:14 +0100

    fix(entity): import organization

--- packages/entity/src/entity.test.ts
+++ packages/entity/src/entity.test.ts
@@ -1,4 +1,4 @@
-import { describe, it, expect } from 'bun:test'
+import { describe, expect, it } from 'bun:test'
 import { Result } from '@theateros/result'
 import * as z from 'zod'
 import { Entity } from './entity'
